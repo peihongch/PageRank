fed36625ee7f29c0efd64f56b68fa7f9e9a09063
171250609 <1782865441@qq.com>
2019-07-03 00:56:23 +0800

    [bugfix] 使用正则表达式修复字符串操作的错误

--- src/main/java/GraphBuilder/GraphBuilderMapper.java
+++ src/main/java/GraphBuilder/GraphBuilderMapper.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * ÊäÈë£º
@@ -12,12 +14,28 @@ import java.io.IOException;
  * ÆÝ³¤·¢ [µÒÔÆ,0.33333|ÆÝ·¼,0.333333|²·Ô«,0.333333]
  * ²·Ô« [µÒÔÆ,0.25|ÆÝ·¼,0.5|ÆÝ³¤·¢,0.25]
  */
-public class GraphBuilderMapper extends Mapper<Text, Text,Text,Text> {
+public class GraphBuilderMapper extends Mapper<Object, Text, Text, Text> {
     @Override
-    protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-        String[] splits = value.toString().split("\t");
-        // Ä¬ÈÏ³õÊ¼PRÊÇ0.5
-        context.write(new Text(splits[0]),
-                new Text("0.5-"+splits[1].substring(1,splits[1].length()-1)));
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        Pattern pattern = Pattern.compile("(\\S+)\\s\\[(.+)\\]");
+        Matcher matcher = pattern.matcher(value.toString());
+        while (matcher.find()) {
+            String a = matcher.group(1);
+            String b = matcher.group(2);
+            // Ä¬ÈÏ³õÊ¼PRÊÇ0.5
+            context.write(new Text(a),
+                    new Text("0.5-" + b));
+        }
+    }
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("(\\S+)\\s\\[(.+)\\]");
+        Matcher matcher = pattern.matcher("µÒÔÆ [ÆÝ·¼,0.33333|ÆÝ³¤·¢,0.333333|²·Ô«,0.333333]");
+        while (matcher.find()) {
+            String a = matcher.group(1);
+            String b = matcher.group(2);
+            System.out.println(a);
+            System.out.println(b);
+        }
     }
 }
--- src/main/java/PageRankIter/PagePankIterMapper.java
+++ src/main/java/PageRankIter/PagePankIterMapper.java
@@ -6,6 +6,8 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 根据权重给不同的链接节点分配PR值
@@ -15,16 +17,34 @@ public class PagePankIterMapper extends Mapper<Text, Text, Text, Text> {
     protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
         String[] splits = value.toString().split("-");
         double curRank = Double.parseDouble(splits[0]);
-        List<String> linkList = Arrays.asList(splits[1].split("[|]"));
+        List<String> linkList = Arrays.asList(splits[1].split("\\|"));
         linkList.forEach(link -> {
-            String[] s = link.split(",");
-            try {
-                context.write(new Text(s[0]), new Text(""+curRank*Double.parseDouble(s[1])));
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-                System.out.println("输出PR值出错！");
+            Pattern pattern = Pattern.compile("(\\S+)[,](\\d+[\\.]\\d+)");
+            Matcher matcher = pattern.matcher(link);
+            while (matcher.find()){
+                try {
+                    context.write(new Text(matcher.group(1)), new Text("" + curRank * Double.parseDouble(matcher.group(2))));
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                    System.out.println("输出PR值出错！");
+                }
             }
         });
         context.write(key, new Text(splits[1]));
+    }
+
+    public static void main(String[] args) {
+        String[] splits = "0.5-戚芳,0.33333|戚长发,0.333333|卜垣,0.333333".split("-");
+        double curRank = Double.parseDouble(splits[0]);
+        List<String> linkList = Arrays.asList(splits[1].split("\\|"));
+        linkList.forEach(link -> {
+            Pattern pattern = Pattern.compile("(\\S+)[,](\\d+[\\.]\\d+)");
+            Matcher matcher = pattern.matcher(link);
+            while (matcher.find()){
+                System.out.println(matcher.group(1));
+                System.out.println(curRank * Double.parseDouble(matcher.group(2)));
+            }
+        });
+        System.out.println(splits[1]);
     }
 }
